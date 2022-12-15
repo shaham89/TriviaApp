faa94609b08eaade7f71bd0ac0996f4ca8ba06e7
shaham89 <shmerom99@gmail.com>
2022-12-15 20:20:17 +0200

    Adding israel questions

---
+++ Israel.py
@@ -0,0 +1,20 @@
+def get_questions_dict():
+    israel_questions_list = [("Who was Israel's first Prime Minister?", "David Ben-Gurion", 'Benjamin Netanyahu', 'Yitzhak Rabin', 'Shimon Peres', 1),
+        ('In what year Israel gained independence', '1947', '1948', '1949', '1946', 2),
+        ('In what year the Six-Day War began?', '1967', '1973', '1970', '1999', 1),
+        ('How long did the Six-Day War lasted for?', '6 weeks', 'one week', '6 days', '6 months', 3),
+        ('When did the Yum-Kippur War began?', '1977', '1948', '1973', '2001', 3),
+        ('Who was the head commander of the IDF during the Six-Day War?', 'Yitzhak Rabin', 'Shimon Peres', 'David Ben-Gurion', 'Moshe Dayan', 1),
+        ('Who served as the president?', 'Yitzhak Rabin', 'Shimon Peres', 'David Ben-Gurion', 'Benjamin Netanyahu', 2),
+        ('Who was assassinated?', 'Yitzhak Rabin', 'Shimon Peres', 'David Ben-Gurion', 'Moshe Dayan', 1),
+        ('What does the IDF initials stand for?', "Israel's Dedicated Force", 'Israel Doom Force', "Israel Defense Forces", "Initiate Doom Force", 3),
+        ("In what year Israel made peace with Egypt?", '1994', '1977', '1973', '1979', 4),
+        ("In what year Israel made peace with Jordan?", '1995', '1994', '1983', '1999', 2),
+        ("Who was the first person in Israel to receive the Noble price?", 'Yosef Agnon', 'Menachem Begin', 'Shimon Peres', 'Yitzhak Rabin', 1),
+        ("Israel was named after which person?", 'Israel', 'Jakob', 'Abraham', 'Israeli', 2),
+                        ()
+    ]
+
+    return israel_questions_list
+
+print(len(get_questions_dict()))
